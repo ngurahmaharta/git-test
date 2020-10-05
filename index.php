394568c714d5c5a5b502646f2ef2886fb278972a
ngurahmaharta <ngurahmahartaa@gmail.com>
2020-10-05 11:41:29 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
     <h1>Hello world</h1>
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur rerum quia nisi voluptatem unde facere sequi a. Illo, a voluptas sequi eius similique sapiente consectetur beatae eligendi tempore soluta modi!</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+        <li>Tiga</li>
+    </ul>
 </body>
 </html>
